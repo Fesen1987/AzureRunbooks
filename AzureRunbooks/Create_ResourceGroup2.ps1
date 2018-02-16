b35a631b9d785d350694a78e78d58e3951d7c796
Fesen1987 <33877599+Fesen1987@users.noreply.github.com>
2018-02-16 13:20:18 +0100

    Updated Create_ResourceGroup2.ps1 in Azure Automation.

--- AzureRunbooks/Create_ResourceGroup2.ps1
+++ AzureRunbooks/Create_ResourceGroup2.ps1
@@ -42,20 +42,23 @@ Connect-AzureAD `
     -CertificateThumbprint $ServicePrincipalConnection.CertificateThumbprint 
 
 # Creating resource group
-#New-AzureRmResourceGroup -Name $ResourceGroupName -Location "West Europe" -Tag @{Name="Owner";Value=$TagOwner}
+New-AzureRmResourceGroup -Name $ResourceGroupName -Location "West Europe" -Tag @{Name="Owner";Value=$TagOwner}
 
 # Adding designated Tags
-#$r = Get-AzureRmResourceGroup -Name $ResourceGroupName
-#$r.tags += @{Name="CostCenter";Value=$TagCostCenter}
-#$r.tags += @{Name="Originator";Value="$TagOriginator"}
-#Set-AzureRmResourceGroup -Name $ResourceGroupName -Tag $r.tags
+$r = Get-AzureRmResourceGroup -Name $ResourceGroupName
+$r.tags += @{Name="CostCenter";Value=$TagCostCenter}
+$r.tags += @{Name="Originator";Value="$TagOriginator"}
+Set-AzureRmResourceGroup -Name $ResourceGroupName -Tag $r.tags
 
 #Finding ADGroups and Azure Roles to assign 
-#$ContributorGroup = Get-AzureRmADGroup -SearchString $Contributors  | Where-Object {$_.SecurityEnabled -eq $true}
-$ContributorGroup = Get-MSOLGroup -SearchString $Contributors  | Where-Object {$_.SecurityEnabled -eq $true}
+$ContributorGroup = Get-AzureADGroup -SearchString $Contributors  | Where-Object {$_.SecurityEnabled -eq $true}
 $ContributorRole = Get-AzureRmRoleDefinition Contributor | Select-Object Name, Description, IsCustom, Id
-$NetworkContributorGroup = Get-AzureRmADGroup -SearchString $NetworkContributors | Where-Object {$_.SecurityEnabled -eq $true}
+$NetworkContributorGroup = Get-AzureADGroup -SearchString $NetworkContributors | Where-Object {$_.SecurityEnabled -eq $true}
 $NetworkContributorRole = Get-AzureRmRoleDefinition "Network Contributor" | Select-Object Name, Description, IsCustom, Id
 
+Write-Output $ContributorGroup 
+Write-Output $ContributorRole
+
 #Assigning the Roles to ADGroups
-#New-AzureRmRoleAssignment -ObjectId $ContributorGroup.Id -Scope "$ResourceGroupName" -RoleDefinitionId $ContributorRole.Id
+New-AzureRmRoleAssignment -ObjectId $ContributorGroup.ObjectId -Scope "/subscriptions/dd91e39a-4a1b-4920-a56a-2da4dd382b11/resourcegroups/$ResourceGroupName" -RoleDefinitionId $ContributorRole.Id
+#New-AzureRmRoleAssignment -ObjectId '9bb39332-6465-4133-82f6-e84dd217e84b' -Scope "/subscriptions/dd91e39a-4a1b-4920-a56a-2da4dd382b11/resourcegroups/BasicServices01-Exchange01" -RoleDefinitionId 'b24988ac-6180-42a0-ab88-20f7382dd24c'
